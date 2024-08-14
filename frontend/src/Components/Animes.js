2b36e1163e82badda180c741f34971c18f66b7b5
Trystan-Muniz <Trystanmuniz@pursuit@gmail.com>
2024-08-14 16:13:52 -0400

    finished

--- frontend/src/Components/Animes.js
+++ frontend/src/Components/Animes.js
@@ -1,11 +1,29 @@
 import Anime from "./Anime";
+import {useEffect, useState} from 'react';
+
 
 function Animes() {
   //fetch all animes from the backend and render them as a list using the Anime component. Make sure to style the animes to look like the screenshot from the README. Feel free to use axios to grab data
 
+const API = process.env.REACT_APP_BASE_URL
+ 
+  const [animes, setAnimes] = useState([]);
+ useEffect(()=> {
+  fetch(`${API}/animes`)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      setAnimes(res)
+    })
+    .catch(err => console.log(err))
+    }, [])
+
+
   return (
     <section className="index" id="anime-list">
-      <p>No animes to display</p>
+       {animes.map((anime) => {})}
+              
+      
     </section>
   );
 }
